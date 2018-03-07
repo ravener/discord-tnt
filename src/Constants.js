9c11f2428abd67f0fbabc9c4bb41252edddabc3f
freetnt5852 <31079629+freetnt5852@users.noreply.github.com>
2018-03-07 17:25:51 +0300

    Add files via upload

--- src/Constants.js
+++ src/Constants.js
@@ -9,6 +9,8 @@ module.exports.VERSION = require("../package.json").version;
 // Library version
 
 module.exports.userAgent = `DiscordBot (https://github.com/freetnt5852/discord-tnt, ${require('../package.json').version})`;
+// User-Agent for http requests.
+
 
 // Gateway Op Codes
 module.exports.GatewayOpCodes = {
@@ -28,6 +30,8 @@ module.exports.GatewayOpCodes = {
 // Code descriptions from official discord api docs
 
 // Voice OpCodes
+// Voice won't be implemented at the moment
+// maybe one day in the future.
 module.exports.VoiceOpCodes = {
 	IDENTIFY: 0, // Begin a voice websocket connection
 	SELECT_PROTOCOL: 1, // Select the voice protocol
@@ -40,15 +44,75 @@ module.exports.VoiceOpCodes = {
 	HELLO: 8, // The continuous interval in milliseconds after which the client should send a heartbeat
 	RESUMED: 9, // Acknowlegde Resume
 	CLIENT_DISCONNECT: 13 // A client has disconnected from the voice channel
-}; 
+};
+
+let PERMISSIONS = {
+  CREATE_INSTANT_INVITE: 0x00000001, // Allows creation of instant invites
+  KICK_MEMBERS: 0x00000002, // Allows kicking members
+  BAN_MEMBERS: 0x00000004, // Allows banning members
+  ADMINISTRATOR: 0x00000008, // Allows all permissions and bypasses channel permission overwrites
+  MANAGE_CHANNELS: 0x00000010, // Allows management and editing of channels
+  MANAGE_GUILD: 0x00000020, // Allows management and editing of the guild
+  ADD_REACTIONS: 0x00000040, // Allows for the addition of reactions to messages
+  VIEW_AUDIT_LOG: 0x00000080, // Allows for viewing of audit logs
+  VIEW_CHANNEL: 0x00000400, // Allows guild members to view a channel, which includes reading messages in text channels
+  SEND_MESSAGES: 0x00000800, // Allows for sending messages in a channel
+  SEND_TTS_MESSAGES: 0x00001000, // Allows for sending of `/tts` messages
+  MANAGE_MESSAGES: 0x00002000, // Allows for deletion of other users messages
+  EMBED_LINKS: 0x00004000, // Links sent by users with this permission will be auto-embedded
+  ATTACH_FILES: 0x00008000, // Allows for uploading images and files
+  READ_MESSAGE_HISTORY: 0x00010000, // Allows for reading of message history
+  MENTION_EVERYONE: 0x00020000, // Allows for using the `@everyone` tag to notify all users in a channel, and the `@here` tag to notify all online users in a channel
+  USE_EXTERNAL_EMOJIS: 0x00040000, // Allows the usage of custom emojis from other servers
+  CONNECT: 0x00100000, // allows for joining of a voice channel
+  SPEAK: 0x00200000, // Allows for speaking in a voice channel
+  MUTE_MEMBERS: 0x00400000, // Allows for muting members in a voice channel
+  DEAFEN_MEMBERS: 0x00800000, // Allows for deafening of members in a voice channel
+  MOVE_MEMBERS: 0x01000000, // Allows for moving of members between voice channels
+  USE_VAD: 0x02000000, // Allows for using voice-activity-detection in a voice channel
+  CHANGE_NICKNAME: 0x04000000, // Allows for modification of own nickname
+  MANAGE_NICKNAMES: 0x08000000, // Allows for modification of other users nicknames
+  MANAGE_ROLES: 0x10000000, // Allows management and editing of roles
+  MANAGE_WEBHOOKS: 0x20000000, // Allows management and editing of webhooks
+  MANAGE_EMOJIS: 0x40000000 // Allows management and editing of emojis
+};
+let arr = Object.values(PERMISSIONS);
+PERMISSIONS.ALL = arr.reduce((x, y) => x | y);
+module.exports.PERMISSIONS = PERMISSIONS;
+
+module.exports.MESSAGE_TYPES = {
+  DEFAULT: 0,
+  RECIPIENT_ADD: 1,
+  RECIPIENT_REMOVE: 2 ,
+  CALL: 3,
+  CHANNEL_NAME_CHANGE: 4,
+  CHANNEL_ICON_CHANGE: 5,
+  CHANNEL_PINNED_MESSAGE: 6,
+  GUILD_MEMBER_JOIN: 7
+};
+ 
 
 module.exports.COLORS = {
 	RED: 0xFF0000,
 	BLUE: 0x0000FF,
 	YELLOW: 0xFFEB00
- // some colors, more coming soon, send a PR if you wanna do it, meant to reach colors easily for embeds..
+ // some colors, more coming soon, send a PR if you wanna do it, meant to reach colors easily for embeds etc..
+ // i'll find better values for colors later and add a
+ // way to use them in embeds.
 }
-// idk if i'll even use this, maybe in the futute.
+
+module.exports.CDN_ENDPOINTS = {
+	avatar: (userID, hash) => {
+		return `https://cdn.discordapp.com/avatars/${userID}/${hash}.${hash.startsWith('a_') ? 'gif' : 'png'}?size=2048`
+	},
+	emoji: (id) => {
+		return `https://cdn.discordapp.com/emojis/${id}`;
+	}
+};
+
+
+
+// idk if i'll even use this, maybe in the future.
 module.exports.ENDPOINTS = {
  channel: (id) => `https://discordapp.com/api/v6/channels/${id}`,
  messages: (channelID) => `https://discordapp.com/api/v6/channels/${channelID}/messages`
